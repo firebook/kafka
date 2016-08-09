d0d21481261cdc89a002d45a2e844a1c5d885ab3
Will Wang Z <will.z.wang@ericsson.com>
2016-08-09 15:52:16 +0800

    US#17874: Add topic creation script and supervision files
    Change-Id: I3e97ddca87cffa5d00cd413038b70c5260d46967

---
+++ kafka/scripts/create-kafka-topic.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Create topic iot-devices-topic
+echo "sleep 30 seconds, wait for zookeeper and kafka startup"
+/bin/sleep 30
+echo "generate iot-devices-topic"
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic iot-devices-topic
