f0ca4b53b822b3611be8bd28302a6c24ff10c2bf
Will Wang Z <will.z.wang@ericsson.com>
2016-08-09 14:35:32 +0800

    US#17874: Update start-kafka.sh, add sleep 20 until zookeeper/kafka startup before create topic
    Change-Id: I5451eef4d1db0624ad14ecb5b1605875fbaa7b72

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -65,6 +65,10 @@ fi
 
 # Run Kafka
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+
 # Create topic iot-devices-topic
+echo "sleep 30 seconds, wait for zookeeper and kafka startup"
+/bin/sleep 30
+echo "generate iot-devices-topic"
 $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic iot-devices-topic
 
